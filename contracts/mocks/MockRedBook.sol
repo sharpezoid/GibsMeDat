84ddbb9125a8ea232e51c6740d897dcfe666750c
sharpezoid <39346203+sharpezoid@users.noreply.github.com>
2025-08-08 00:56:08 +0100

    Use Address library for DAO payouts and test gas-heavy recipients

---
+++ contracts/mocks/MockRedBook.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+/// @title MockRedBook
+/// @notice Minimal ERC1155 token with minting capability for tests.
+contract MockRedBook is ERC1155 {
+    constructor() ERC1155("") {}
+
+    function mint(address to, uint256 id, uint256 amount) external {
+        _mint(to, id, amount, "");
+    }
+}
